69cceb241e77661f68f6533c68a375bc31ee5b1b
Sean Carey <seancarey18@gmail.com>
2018-10-12 15:27:00 -0400

    Fix regex problem

--- wtsil/settings.py
+++ wtsil/settings.py
@@ -126,6 +126,5 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
         'LOCATION': '/var/tmp/django_cache',
-        'TIMEOUT': 60 * 60,
     }
 }
