70bbdd6c8d36c5c6e28f8ff77b08d0f521dff877
Sean Carey <seancarey18@gmail.com>
2018-10-12 01:33:50 -0400

    Add github search plus considerable refactoring

--- main/apicalls.py
+++ main/apicalls.py
@@ -2,7 +2,20 @@ import xml.etree.ElementTree as ET
 import aiohttp
 import asyncio
 
-from .techlist import TechCounter, TECHS
+from .techlist import TechCounter, new_tech_list
+
+
+CATEGORIES = [
+	'Languages',
+	'Frameworks/CMS',
+	'Platforms/Web Services',
+	'Libraries/Tools',
+	'Game Engines',
+	'Concepts/Skills',
+	'Operating Systems',
+	'Databases',
+	'Other uncategorized keywords',
+]
 
 
 def run_searches(request):
@@ -14,6 +27,7 @@ def run_searches(request):
 async def search_all(loop, request):
 	searches = [
 		search_stackoverflow,
+		search_github,
 	]
 	async with aiohttp.ClientSession(loop=loop) as session:
 		results = [search(session, request) for search in searches]
@@ -21,57 +35,62 @@ async def search_all(loop, request):
 
 async def search_stackoverflow(session, request):
 	location = request.GET['search']
-	async with session.get('https://stackoverflow.com/jobs/feed', params={'location': location}) as resp:
+	base_url = 'https://stackoverflow.com/jobs/feed'
+	async with session.get(base_url, params={'location': location}) as resp:
 		root = ET.fromstring(await resp.text())
-		keywords = {}
+		categories = {category:[] for category in CATEGORIES}
 		job_posts = [x for x in root[0] if x.tag == 'item']
 		for post in job_posts:
-			categories = [x for x in post if x.tag == 'category']
-			for category in categories:
-				try: 
-					keywords[category.text] += 1
-				except KeyError:
-					keywords[category.text] = 1
-		return keywords
+			keywords = [x for x in post if x.tag == 'category']
+			for keyword in keywords:
+				categories = find_match(categories, keyword.text)
+		return categories
+
+async def search_github(session, request):
+	location = request.GET['search']
+	categories = {category:[] for category in CATEGORIES}
+	base_url = 'https://jobs.github.com/positions.json'
+	async with session.get(base_url, params={'location': location}) as resp:
+		results = await resp.json()
+		for post in results:
+			categories = find_match(categories, post['description'], create_new=False)
+		return categories
+
+def find_match(categories, text, create_new=True):
+	match = False
+	techs = new_tech_list()
+	for tech in techs:
+		if tech.match(text):
+			tech.count += 1
+			match = True
+			if tech not in categories[tech.category]:
+				categories[tech.category].append(tech)
+	if not match and create_new:
+		new_tech = TechCounter(text, create_regex(text), 'Other uncategorized keywords', count=1)
+		categories['Other uncategorized keywords'].append(new_tech)
+		techs.append(new_tech)
+	return categories
 
 def aggregate_results(search_results):
-	categories = {
-		'Languages': [],
-		'Frameworks/CMS': [],
-		'Platforms/Web Services': [],
-		'Libraries/Tools': [],
-		'Game Engines': [],
-		'Concepts/Skills': [],
-		'Operating Systems': [],
-		'Databases': [],
-		'Other uncategorized keywords': [],
-	}
+	categories = {category:[] for category in CATEGORIES}
 	for results in search_results:
 		for key in results:
-			match = False
-			for tech in TECHS:
-				if tech.match(key):
-					tech.count += results[key]
-					match = True
-					if tech not in categories[tech.category]:
-						categories[tech.category].append(tech)
-			if not match:
-				new_tech = TechCounter(key, create_regex(key), 'Other uncategorized keywords', count=results[key])
-				categories['Other uncategorized keywords'].append(new_tech)
-				TECHS.append(new_tech)
-
-	for c in categories:
-		categories[c] = sorted(categories[c], key=lambda tech: -tech.count)
-
+			for tech in results[key]:
+				shared_techs = [t for t in categories[key] if t.name == tech.name]
+				if len(shared_techs) == 0:
+					categories[key].append(tech)
+				else:
+					shared_techs[0].count += tech.count
+	for key in categories:
+		categories[key] = sorted(categories[key], key=lambda tech: -tech.count)
 	return categories
 
 def create_regex(string):
 	tokens = '-.?+*,$'
-	regex = r'^'
+	regex = r'^(.+?\W)?'
 	for char in string:
 		if char in tokens:
 			regex += f'\{char}'
 		else:
 			regex += char
-	return regex + '$'
-
+	return regex + r'(\W.+?)?$'
