9cb4d99caa39d1f356eec2f8fd69882409569e07
Sean Carey <seancarey18@gmail.com>
2018-10-29 16:42:02 -0400

    Style changes to home page, add temporary no results page

--- main/views.py
+++ main/views.py
@@ -14,6 +14,7 @@ def home(request):
 			cache.set(search, results, 60 * 60 * 12)
 		else:
 			print('Retrieved from cache')
+		template = 'main/search.html' if max((len(results[x]) for x in results)) > 0 else 'main/no_results.html'
 		context = {'search': search, 'results': results}
-		return render(request, 'main/search.html', context)
+		return render(request, template, context)
 	return render(request, 'main/home.html')
