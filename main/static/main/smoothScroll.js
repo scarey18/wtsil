2cb1d6564c8866eece995c0123db4f7d75ab7742
Sean Carey <seancarey18@gmail.com>
2018-11-14 16:55:37 -0500

    Add site logo

--- main/static/main/smoothScroll.js
+++ main/static/main/smoothScroll.js
@@ -1,12 +1,21 @@
+const homeLogo = document.querySelector('#home-logo');
+const hero = document.querySelector('.hero');
 const arrowDown = document.querySelector('#arrow-down');
 const arrowUp = document.querySelector('#arrow-up');
-const hero = document.querySelector('.hero');
+const fixedArrow = document.querySelector('.fixed-arrow');
 
+window.addEventListener('scroll', () => {
+	if (window.scrollY > window.innerHeight) fixedArrow.style.visibility = 'visible';
+	else fixedArrow.style.visibility = 'hidden';
+});
 
-arrowDown.addEventListener('click', () => {
-	window.scrollTo({
-		top: window.scrollY + hero.getBoundingClientRect().top,
-		behavior: 'smooth',
+[homeLogo, arrowDown].forEach(element => {
+	if (!element) return;
+	element.addEventListener('click', () => {
+		window.scrollTo({
+			top: window.scrollY + hero.getBoundingClientRect().top,
+			behavior: 'smooth',
+		});
 	});
 });
 
