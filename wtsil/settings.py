323058cbb298636d5f3ccc5b89c1f804f00334a6
Sean Carey <seancarey18@gmail.com>
2018-11-16 19:29:08 -0500

    Remove unnecessary middleware and files from project

--- wtsil/settings.py
+++ wtsil/settings.py
@@ -31,22 +31,15 @@ ALLOWED_HOSTS = ['192.168.9.3']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
